a490185411f3a0a830306fa51c7ee8a02da73adc
Domenico Gemoli <domenicogemoli@gmail.com>
2018-02-12 23:57:23 +0100

    quadratic fitness

--- src/phrase.ts
+++ src/phrase.ts
@@ -4,7 +4,7 @@ import IGene from './lib/IGene';
 import { Organism } from './lib/Organism';
 import Population from './lib/Population';
 
-const target = 'To be or not to be';
+const target = 'To be or not to be that is the question.';
 const populationSize = 200;
 const mutationRate = 0.01;
 
@@ -20,12 +20,13 @@ class CharGene implements IGene {
 // tslint:disable-next-line: max-classes-per-file
 class PhraseOrganism extends Organism {
 	protected calculateFitness(): number {
-		return this.genes.reduce((fitness, gene, i) => {
+		const simpleFitness = this.genes.reduce((fitness, gene, i) => {
 			if (target.charAt(i) !== gene.value) {
 				return fitness;
 			}
 			return fitness + 1;
 		}, 0);
+		return Math.pow(simpleFitness, 4);
 	}
 
 	protected getRandomGene(): IGene {
